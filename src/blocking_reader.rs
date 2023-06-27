e892c003af4b0d47a820894cd635021407686edc
Florentin Schäfer <florentin.schaefer@gmail.com>
2023-06-28 01:12:33 +0200

    improve code readability

--- src/blocking_reader.rs
+++ src/blocking_reader.rs
@@ -1,12 +1,12 @@
-use std::{io, thread, time::Duration};
+use std::{io::{self, Read}, thread, time::Duration};
 
 // Source: https://github.com/ivmarkov/rust-esp32-std-demo/issues/59#issuecomment-1030744674
-pub struct BlockingReader<R: io::Read> {
-    poll: core::time::Duration,
+pub struct BlockingReader<R: Read> {
+    poll: Duration,
     reader: R,
 }
 
-impl<R: io::Read> From<R> for BlockingReader<R> {
+impl<R: Read> From<R> for BlockingReader<R> {
     fn from(reader: R) -> Self {
         Self {
             poll: Duration::from_millis(10),
@@ -15,7 +15,7 @@ impl<R: io::Read> From<R> for BlockingReader<R> {
     }
 }
 
-impl<R: io::Read> io::Read for BlockingReader<R> {
+impl<R: Read> Read for BlockingReader<R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         if buf.is_empty() {
             return Ok(0);
