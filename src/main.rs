167be7d26c6309120d6f627ca0c4f8d208e84a6a
Florentin Schäfer <florentin.schaefer@gmail.com>
2023-08-25 19:09:17 +0200

    Replace tokio with os threads The sync feature from tokio failed to compile with an error from ldproxy

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,16 @@ use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::prelude::*;
 use log::*;
 use scd30::Scd30;
-use std::io::{self, BufRead, BufReader};
-use tokio::{
-    sync::broadcast,
-    task,
-    time::{sleep, Duration},
+use crossbeam_channel::unbounded;
+use std::{
+    io::{self, BufRead, BufReader},
+    thread::{self, sleep},
+    time::Duration,
 };
 
 mod blocking_reader;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
@@ -22,50 +21,45 @@ async fn main() {
 
     let peripherals = Peripherals::take().unwrap();
     let i2c = peripherals.i2c0;
-    let sda = peripherals.pins.gpio7;
-    let scl = peripherals.pins.gpio6;
+    let sda = peripherals.pins.gpio6;
+    let scl = peripherals.pins.gpio7;
 
     let config = I2cConfig::new().baudrate(400.kHz().into());
     let i2c = I2cDriver::new(i2c, sda, scl, &config).unwrap();
 
     let mut scd30 = Scd30::new(i2c);
 
-    let (request_tx, mut request_rx) = broadcast::channel::<()>(5);
-    let (reply_tx, _) = broadcast::channel(5);
+    let (request_tx, request_rx) = unbounded();
+    let (reply_tx, reply_rx) = unbounded();
 
-    task::spawn({
-        let reply_tx = reply_tx.clone();
-        async move {
-            loop {
-                request_rx.recv().await.unwrap();
-                reply_tx.send(scd30.read()).unwrap();
-            }
+    let local_reply_tx = reply_tx.clone();
+    let data_thread = thread::spawn(move || {
+        loop {
+            request_rx.recv().unwrap();
+            local_reply_tx.send(scd30.read()).unwrap();
         }
     });
 
-    task::spawn({
-        let reply_tx = reply_tx.clone();
-        let request_tx = request_tx.clone();
-        async move {
-            loop {
-                let mut reply_rx = reply_tx.subscribe();
-                request_tx.send(()).unwrap();
+    let local_reply_rx = reply_rx.clone();
+    let local_request_tx = request_tx.clone();
+    let info_thread = thread::spawn(move || {
+        loop {
+            local_request_tx.send(()).unwrap();
 
-                match reply_rx.recv().await.unwrap() {
-                    Ok(reading) => match reading {
-                        Some(measurement) => {
-                            info!("Automatic measurement: {:?}", measurement);
-                            sleep(Duration::from_secs(60 * 10)).await;
-                        }
-                        None => {
-                            info!("Automatic measurement: No data is available from the sensor, waiting 5 seconds");
-                            sleep(Duration::from_secs(5)).await;
-                        }
-                    },
-                    Err(_) => {
-                        info!("Automatic measurement: Sensor not ready, waiting 5 seconds");
-                        sleep(Duration::from_secs(5)).await;
+            match local_reply_rx.recv().unwrap() {
+                Ok(reading) => match reading {
+                    Some(measurement) => {
+                        info!("Automatic measurement: {:?}", measurement);
+                        sleep(Duration::from_secs(60 * 10)).await;
                     }
+                    None => {
+                        info!("Automatic measurement: No data is available from the sensor, waiting 5 seconds");
+                        sleep(Duration::from_secs(5));
+                    }
+                },
+                Err(_) => {
+                    info!("Automatic measurement: Sensor not ready, waiting 5 seconds");
+                    sleep(Duration::from_secs(5));
                 }
             }
         }
@@ -79,8 +73,7 @@ async fn main() {
         line.clear();
         let _ = stdin.read_line(&mut line).unwrap();
 
-        let mut reply_rx = reply_tx.subscribe();
-        request_tx.send(()).unwrap();
-        println!("{:#?}", reply_rx.recv().await.unwrap());
+        request_tx.send(());
+        println!("{:#?}", reply_rx.recv().unwrap());
     }
 }
