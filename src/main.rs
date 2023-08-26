b27daf81860908f22a6e2bbfd4f9fb5e542a6a03
Florentin Schäfer <florentin.schaefer@gmail.com>
2023-08-26 23:38:45 +0200

    Added ccs811 sensor (todo: rework for modularity)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use crossbeam_channel::bounded;
-use esp_idf_hal::i2c::*;
-use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::prelude::*;
-use log::*;
-use scd30::Scd30;
 use std::{
     io::{self, BufRead, BufReader},
     thread::{self, sleep},
     time::Duration,
 };
+
+use crossbeam_channel::bounded;
+
+use esp_idf_hal::i2c::*;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::prelude::*;
+use log::*;
+
+use embedded_ccs811::{nb::block, prelude::*, Ccs811Awake};
+use scd30::Scd30;
 
 mod blocking_reader;
 
@@ -19,15 +23,37 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    // Setup I2C
     let peripherals = Peripherals::take().unwrap();
     let i2c = peripherals.i2c0;
     let sda = peripherals.pins.gpio6;
     let scl = peripherals.pins.gpio7;
-
     let config = I2cConfig::new().baudrate(400.kHz().into());
     let i2c = I2cDriver::new(i2c, sda, scl, &config).unwrap();
+    let i2c: &'static _ = shared_bus::new_std!(I2cDriver = i2c).unwrap();
 
-    let mut scd30 = Scd30::new(i2c);
+    // Setup scd-30 sensor
+    let mut scd30 = Scd30::new(i2c.acquire_i2c());
+    // Setup ccs-811 sensor
+    let mut ccs811 = loop {
+        let ccs811 = Ccs811Awake::new(i2c.acquire_i2c(), embedded_ccs811::SlaveAddr::default());
+        match ccs811.start_application() {
+            Ok(mut new_ccs811) => {
+                match new_ccs811.set_mode(embedded_ccs811::MeasurementMode::ConstantPower1s) {
+                    Ok(_) => break new_ccs811,
+                    Err(error) => {
+                        error!("{:?}", error);
+                        continue;
+                    }
+                }
+            }
+            Err(embedded_ccs811::ModeChangeError { dev: _, error }) => {
+                error!("{:?}", error);
+                continue;
+            }
+        }
+    };
+
 
     let (request_tx, request_rx) = bounded(5);
     let (reply_tx, reply_rx) = bounded(5);
@@ -35,7 +61,7 @@ fn main() {
     let local_reply_tx = reply_tx.clone();
     thread::spawn(move || loop {
         request_rx.recv().unwrap();
-        local_reply_tx.send(scd30.read()).unwrap();
+        local_reply_tx.send((scd30.read(), block!(ccs811.data()))).unwrap();
     });
 
     let local_reply_rx = reply_rx.clone();
@@ -43,22 +69,12 @@ fn main() {
     thread::spawn(move || loop {
         local_request_tx.send(()).unwrap();
 
-        match local_reply_rx.recv().unwrap() {
-            Ok(reading) => match reading {
-                Some(measurement) => {
-                    info!("Automatic measurement: {:?}", measurement);
-                    sleep(Duration::from_secs(60 * 10));
-                }
-                None => {
-                    info!("Automatic measurement: No data is available from the sensor, waiting 5 seconds");
-                    sleep(Duration::from_secs(5));
-                }
-            },
-            Err(_) => {
-                info!("Automatic measurement: Sensor not ready, waiting 5 seconds");
-                sleep(Duration::from_secs(5));
-            }
-        }
+        
+        let (scd30_data, ccs811_data) = local_reply_rx.recv().unwrap();
+        handle_scd30_measurement(scd30_data);
+        handle_ccs811_measurement(ccs811_data);
+
+        sleep(Duration::from_secs(60 * 10));
     });
 
     let stdin: blocking_reader::BlockingReader<_> = io::stdin().into();
@@ -71,5 +87,28 @@ fn main() {
 
         request_tx.send(()).unwrap();
         println!("{:#?}", reply_rx.recv().unwrap());
+    }
+}
+
+fn handle_scd30_measurement(reading: Result<Option<scd30::Measurement>, I2cError>) {
+    match reading {
+        Ok(reading) => match reading {
+            Some(measurement) => {
+                info!("scd30: {:?}", measurement);
+            }
+            None => {
+                info!("scd30: No data is available from the sensor");
+            }
+        },
+        Err(err) => {
+            info!("scd30: Sensor error: {:?}", err);
+        }
+    }
+}
+
+fn handle_ccs811_measurement(reading: Result<embedded_ccs811::AlgorithmResult, embedded_ccs811::ErrorAwake<I2cError>>) {
+    match reading {
+        Ok(data) => info!("ccs811: {:?}", data),
+        Err(err) => info!("ccs811: Sensor error: {:?}", err),
     }
 }
